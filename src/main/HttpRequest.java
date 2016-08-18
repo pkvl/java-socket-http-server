a7f9f35464745f1bf7894d12de9fde682a085b9a
anroypaul <anroypaul@gmail.com>
2016-08-18 20:46:58 +0300

    Changed try-with-resources

--- src/main/HttpRequest.java
+++ src/main/HttpRequest.java
@@ -32,59 +32,55 @@ public class HttpRequest {
         System.out.println("Listening for connection on port " + PORT + "...");
 
         while (true) {
-            try (Socket socket = server.accept()) {
-                // Initialize reader and writer
-                try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                    try (PrintWriter out = new PrintWriter(socket.getOutputStream())) {
-                        // Start sending reply, using the HTTP 1.1 protocol
-                        out.print("HTTP/1.1 200 \r\n");
-                        out.print("Content-Type: text/plain\r\n");
-                        out.print("Connection: close\r\n");
-                        out.print("\r\n");
+            try (Socket socket = server.accept();
+                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 PrintWriter out = new PrintWriter(socket.getOutputStream()))
+            // Initialize reader and writer
+            {
+                // Start sending reply, using the HTTP 1.1 protocol
+                out.print("HTTP/1.1 200 \r\n");
+                out.print("Content-Type: text/plain\r\n");
+                out.print("Connection: close\r\n");
+                out.print("\r\n");
 
-                        // Read HTTP request from the client
-                        // Get HttpRequest lines
-                        String parts[] = getRequest(in, out);
+                // Read HTTP request from the client
+                // Get HttpRequest lines
+                String parts[] = getRequest(in, out);
 
-                        int id = 0;
-                        //String httpResponse = null;
+                int id = 0;
+                //String httpResponse = null;
 
-                        try {
-                            method = parts[0];
-                            method = method.substring(0, method.length() - 1); // Remove space
-                            //System.out.println(method + " " + symbols);
+                try {
+                    method = parts[0];
+                    method = method.substring(0, method.length() - 1); // Remove space
+                    //System.out.println(method + " " + symbols);
 
-                            // Return HTTP response with info about user, stored in DB
-                            if (GET.equals(method)) {
-                                symbols = parts[1];
-                                parts = symbols.split(" ");
+                    // Return HTTP response with info about user, stored in DB
+                    if (GET.equals(method)) {
+                        symbols = parts[1];
+                        parts = symbols.split(" ");
 
-                                // Check if ID is integer
-                                if (ParseChecker.tryParseInt(parts[0]))
-                                    id = new Integer(parts[0]);
-                                setHttpResponse(new DbWork().getUserInfo(id));
-                            } else if (POST.equals(method)) {
-                                symbols = parts[1];
-                                if (ParseChecker.tryParseInt(parts[1]))
-                                    id = new Integer(parts[1]);
-                                String name = parts[2];
-                                String surname = parts[3];
-                                setHttpResponse(new DbWork().addNewUser(new User(id, name, surname)));
-                            }
-                            //System.out.println(id);
-                        } catch (NumberFormatException e) {
-                            System.out.println("Invalid id number");
-                        }
-
-                        out.print(getHttpResponse()); // Print info about user
-                        out.close(); // Flush and close the output stream
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                        // Check if ID is integer
+                        if (ParseChecker.tryParseInt(parts[0]))
+                            id = new Integer(parts[0]);
+                        setHttpResponse(new DbWork().getUserInfo(id));
+                    } else if (POST.equals(method)) {
+                        symbols = parts[1];
+                        if (ParseChecker.tryParseInt(parts[1]))
+                            id = new Integer(parts[1]);
+                        String name = parts[2];
+                        String surname = parts[3];
+                        setHttpResponse(new DbWork().addNewUser(new User(id, name, surname)));
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    //System.out.println(id);
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid id number");
                 }
-                //in.close(); // Close the input stream
+
+                out.print(getHttpResponse()); // Print info about user
+                out.close(); // Flush and close the output stream
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
